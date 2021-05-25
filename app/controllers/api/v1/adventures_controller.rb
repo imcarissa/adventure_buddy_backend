9234adca8969ebdcc26539dbc2854c2600582859
imcarissa <carissa.sarreal@gmail.com>
2021-05-25 03:18:07 -0400

    namespaced and nested index and create route for adventures

--- app/controllers/api/v1/adventures_controller.rb
+++ app/controllers/api/v1/adventures_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::AdventuresController < ApplicationController
+
+    def index
+        adventures = Adventure.all
+        render json: adventures
+    end
+
+    def create
+        adventure = Adventure.new(adventure_params)
+        if adventure.save
+            render json: adventure, status: :accepted
+        else
+            render json: {errors: adventure.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def adventure_params
+        params.require(:adventure).permit(:title, :location, :description, :image_url, :category_id)
+    end
+
+
 end

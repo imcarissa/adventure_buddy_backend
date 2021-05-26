f6260a9afa64d309fcdfe7b8f63c3b3d6588da5b
imcarissa <carissa.sarreal@gmail.com>
2021-05-26 04:12:26 -0400

    created serializers, added serializers to index route

--- app/serializers/adventure_serializer.rb
+++ app/serializers/adventure_serializer.rb
@@ -1,4 +1,4 @@
 class AdventureSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :location, :description, :image_url, :category_id
+  attributes :title, :location, :description, :image_url, :category_id, :category 
 end
